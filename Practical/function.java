a325db3cdd1191170012da6efb6602f5ac5cb603
samprabhu129 <112237351+samprabhu129@users.noreply.github.com>
2023-01-20 18:02:50 +0530

    Add files via upload

---
+++ Practical/function.java
@@ -0,0 +1,16 @@
+class My
+{
+	public static void Display()
+	{
+	System.out.println("Hi....");
+	}
+}
+class function
+{
+public static void main(String args[])
+{//entry point
+	System.out.println("i am learning java");//will point on console
+	
+	My.Display();//classname.methodname
+}
+}//end of class

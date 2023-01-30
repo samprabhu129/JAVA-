dfc3562cb8adfb5523930999395b8c4ec1723285
samprabhu129 <112237351+samprabhu129@users.noreply.github.com>
2023-01-30 18:36:20 +0530

    Add files via upload

---
+++ Practical/Simple.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Simple{
+	public static void main(String Args[]){
+		int x=12;
+		int y=13;
+		if(x>y)
+		{
+			System.out.println("x is greater than y");
+		}
+		else
+		{
+			System.out.println("x is not greater than y");
+		}
+	}
+}

3a6a5ebc60756ab3c409b3aa6a1420f2392382af
samprabhu129 <112237351+samprabhu129@users.noreply.github.com>
2023-01-31 17:52:18 +0530

    Add files via upload

---
+++ Practical/NestedForExample.java
@@ -0,0 +1,16 @@
+class NestedForExample{
+	
+	public static void main(String args[]){
+		int rows =5;
+		
+		//outer loop
+		for(int i =1;i<=rows;i++){
+			
+		//inner loop
+		for(int j=1;j<=i;++j){//loop to print number
+			System.out.print(j+ "");
+		}
+		System.out.println("");
+		}
+	}
+}

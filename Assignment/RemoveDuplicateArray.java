fc41d03981e58022cdd2940776e5c48a5cd371fc
samprabhu129 <112237351+samprabhu129@users.noreply.github.com>
2023-02-22 14:26:00 +0530

    Add files via upload

---
+++ Assignment/RemoveDuplicateArray.java
@@ -0,0 +1,24 @@
+package practice;
+
+public class RemoveDuplicateArray {
+	public static int removeDuplicate(int arr[],int n) {
+		if(n==0||n==1) {
+			return n;
+		}
+		int[] temp = new int[n];
+		int j=0;
+		for(int i =0;i<n-1;i++) {
+			
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {10,20,5,22,66,35,10,5,70,68};
+		int l = arr.length;
+		l = removeDuplicate(arr,l);
+
+	}
+
+}
